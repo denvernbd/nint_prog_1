20a77f65de727bba73eaefcaffbd55f5d824a8ee
Денис Васильев <denisvasiliev@dmbp.local>
2016-04-30 02:27:16 +0300

    Initial Commit

---
+++ nint_prog_1/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  nint_prog_1
+//
+//  Created by Денис Васильев on 30/04/16.
+//  Copyright © 2016 dennis. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
